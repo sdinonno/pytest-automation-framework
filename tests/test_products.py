0698ba3e19ac55bd35ea3c95b18ecf9f726a07c8
Sofia Di Nonno <sofia@thelucky.app>
2023-06-18 18:21:54 -0300

    New test for products

--- tests/test_products.py
+++ tests/test_products.py
@@ -6,3 +6,9 @@ def test_get_products_list():
     endpoint =   FileClass.get_property_from_file('PRODUCTS_LIST')
     response = APIClient().get(endpoint)
     assert response.status_code == 200
+
+def test_post_search_product_without_param():
+    endpoint = FileClass.get_property_from_file('SEARCH')
+    response = APIClient().post(endpoint)
+    assert response.status_code == 200
+    assert response.json()["message"] == 'Bad request, search_product parameter is missing in POST request.'
