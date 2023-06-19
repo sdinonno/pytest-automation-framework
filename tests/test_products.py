2d8c84c36a9d429ef79265b35f03de2b52e749dd
Sofia Di Nonno <sofia@thelucky.app>
2023-06-19 11:30:50 -0300

    New test for all products list

--- tests/test_products.py
+++ tests/test_products.py
@@ -1,11 +1,16 @@
 from api.client import APIClient
 from helper.helper import FileClass
-import pytest
 
 def test_get_products_list():
     endpoint =   FileClass.get_property_from_file('PRODUCTS_LIST')
     response = APIClient().get(endpoint)
     assert response.status_code == 200
+
+def test_post_in_product_list():
+    endpoint =   FileClass.get_property_from_file('PRODUCTS_LIST')
+    response = APIClient().post(endpoint)
+    assert response.status_code == 200
+    assert response.json()["message"] == 'This request method is not supported.'
 
 def test_post_search_product_without_param():
     endpoint = FileClass.get_property_from_file('SEARCH')
