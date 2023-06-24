95f776bf1ea70dfa9487c57452b74a054107875c
Sofia Di Nonno <sofia@thelucky.app>
2023-06-24 10:26:15 -0300

    New const BRANDS_END to reuse in all the tests and a new validation for one test

--- tests/test_brands.py
+++ tests/test_brands.py
@@ -1,13 +1,14 @@
 from api.client import APIClient
 from helper.helper import FileClass
 
+BRANDS_END = FileClass.get_property_from_file('BRANDS')
+
 def test_get_all_brands_list():
-    endpoint = FileClass.get_property_from_file('BRANDS')
-    response = APIClient().get(endpoint)
+    response = APIClient().get(BRANDS_END)
     assert response.status_code == 200
+    assert len(response.json()["brands"]) > 0
 
 def test_put_all_brands_list():
-    endpoint = FileClass.get_property_from_file('BRANDS')
-    response = APIClient().post(endpoint)
+    response = APIClient().post(BRANDS_END)
     assert response.status_code == 200
     assert response.json()["message"] == 'This request method is not supported.'
