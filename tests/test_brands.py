3af45a72444959dcb48eaaeb297730885483f9d9
Sofia Di Nonno <sofia@thelucky.app>
2023-06-24 13:44:35 -0300

    Minor changes in all the tests

--- tests/test_brands.py
+++ tests/test_brands.py
@@ -6,9 +6,9 @@ BRANDS_END = FileClass.get_property_from_file('BRANDS')
 def test_get_all_brands_list():
     response = APIClient().get(BRANDS_END)
     assert response.status_code == 200
-    assert len(response.json()["brands"]) > 0
+    assert len(response.json()["brands"]) > 0, 'List of brands is empty.'
 
 def test_put_all_brands_list():
     response = APIClient().post(BRANDS_END)
     assert response.status_code == 200
-    assert response.json()["message"] == 'This request method is not supported.'
+    assert response.json()["message"] == 'This request method is not supported.', 'Error message does not match.'
--- tests/test_login.py
+++ tests/test_login.py
@@ -7,7 +7,6 @@ LOGIN = FileClass.get_property_from_file('LOGIN')
 @pytest.mark.parametrize("params, message", [
     ({"email": "sofiditest1@gmail.com"}, 'Bad request, email or password parameter is missing in POST request.'),
     ({"password": "Admin123"}, 'Bad request, email or password parameter is missing in POST request.'),
-    ({"email": "sofiditest1@gmail.com", "password": "Admin123"}, 'User not found!')
 ])
 
 def test_post_login_with_invalid_details(params, message):
