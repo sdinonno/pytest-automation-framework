57b4225c43ca50e62e3a067438ac708cb9f48982
Sofia Di Nonno <sofidinonno@gmail.com>
2025-08-05 16:29:46 +0200

    New login scenarios

--- tests/integration/test_login.py
+++ tests/integration/test_login.py
@@ -4,12 +4,29 @@ from helper.helper import FileClass
 
 LOGIN = FileClass.get_property_from_file('LOGIN')
 
+client = APIClient()
+pytestmark = [pytest.mark.api, pytest.mark.login]
+
 @pytest.mark.parametrize("params, message", [
     ({"email": "sofiditest1@gmail.com"}, 'Bad request, email or password parameter is missing in POST request.'),
     ({"password": "Admin123"}, 'Bad request, email or password parameter is missing in POST request.'),
 ])
 
-def test_post_login_with_invalid_details(params, message):
-    response = APIClient().post(LOGIN, data=params)
+def test_post_login_without_parameter(params, message):
+    response = client.post(LOGIN, data=params)
     assert response.json()["message"] == message
+    
+@pytest.mark.parametrize("params, message", [
+    ({"email": "sofiditest1@gmail.com", "password": "AAAAA"}, 'User not found!')])
+def test_post_login_with_invalid_credentials(params, message):
+    response = client.post(LOGIN, data=params)
+    assert response.status_code == 200
+    assert response.json().get("message") == message
+
+@pytest.mark.parametrize("params, message", [
+    ({"email": "ontopcontract@gmail.com", "password": "Admin@123"}, 'User exists!')])
+def test_post_login_with_valid_details(params, message):
+    response = client.post(LOGIN, data=params)
+    assert response.status_code == 200
+    assert response.json().get("message") == message
     
